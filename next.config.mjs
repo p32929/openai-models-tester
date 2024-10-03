5751e0b9c97e262b1de74c023f23ec232f8037d5
Fayaz <fayaz@herogram.app>
2024-10-03 11:33:51 +0400

    updted next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
+// Handle unhandled promise rejections
+process.on('unhandledRejection', error => {
+  console.log('unhandledRejection', error);
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // onDemandEntries: {
+  //   // period (in ms) where the server will keep pages in the buffer
+  //   maxInactiveAge: 60 * 60 * 1000,
+  //   // number of pages that should be kept simultaneously without being disposed
+  //   pagesBufferLength: 5,
+  // },
 };
 
 export default nextConfig;
