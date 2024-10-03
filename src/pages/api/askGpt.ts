2fb89d9cd22f96c8593beea3615f6051ae70e959
Fayaz <fayaz@herogram.app>
2024-10-03 11:28:22 +0400

    fixed api keys

--- src/pages/api/askGpt.ts
+++ src/pages/api/askGpt.ts
@@ -2,15 +2,8 @@ import { SimplerAllAis } from '@/utils/simpler-all-ais';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const API_KEY = process.env.OPENAI_API_KEY; // Fetch the OpenAI API key from environment variable
-
     if (req.method === 'POST') {
         const { prompt, data, previousConversations, model, apiKey } = req.body;
-
-        // Check if the provided OpenAI API key matches the one in the environment variable
-        if (apiKey !== API_KEY) {
-            return res.status(401).json({ success: false, message: 'Unauthorized: Invalid API Key' });
-        }
 
         try {
             const response = await SimplerAllAis.askGpt({
